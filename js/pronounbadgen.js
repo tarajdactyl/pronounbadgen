7a9e54941631e83b2df79db277dce0ef8a9b5b32
TJ Evans-Ellis <thomasjayellis@gmail.com>
2023-09-09 21:04:37 -0400

    add pretty buttons when flags are selected

--- js/pronounbadgen.js
+++ js/pronounbadgen.js
@@ -6,17 +6,14 @@ function init(){
 function updatePronouns(e){
     document.getElementById("pronouns").textContent = e.target.value
 }
-function set_flag1(flagid){
-    document.getElementById("flag1").attributes['xlink:href'].value = flagid
-}
-
-function set_flag2(flagid){
-    document.getElementById("flag2").attributes['xlink:href'].value = flagid
+function set_flag(idx, flagid){
+    document.getElementById("flag"+idx).attributes['xlink:href'].value = "#" + flagid
+    document.getElementById("flag"+idx+"input").style.background = "url(svg/"+flagid+".svg) repeat-x"
 }
 
 function switch_flags(){
-    f1 = document.getElementById("flag1").attributes['xlink:href'].value
-    f2 = document.getElementById("flag2").attributes['xlink:href'].value
-    document.getElementById("flag1").attributes['xlink:href'].value = f2
-    document.getElementById("flag2").attributes['xlink:href'].value = f1
+    f1 = document.getElementById("flag1").attributes['xlink:href'].value.slice(1)
+    f2 = document.getElementById("flag2").attributes['xlink:href'].value.slice(1)
+    set_flag(1,f2)
+    set_flag(2,f1)
 }
