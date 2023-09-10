b4b2729b5dae773cc239f8732cabde95bc28332d
TJ Evans-Ellis <thomasjayellis@gmail.com>
2023-09-09 23:01:00 -0400

    auto-populate dropdowns
     - add pan flag
     - tweak slash to match angle of the '/' character
     - add javascript to populate the dropdowns, greatly cutting down on
       copy/pasted code

--- js/pronounbadgen.js
+++ js/pronounbadgen.js
@@ -2,13 +2,14 @@ function init(){
     pinput = document.getElementById("pronouninput")
     updatePronouns({target:pinput})
     pinput.addEventListener("input", updatePronouns) 
+    build_flag_dropdowns()
 }
 function updatePronouns(e){
     document.getElementById("pronouns").textContent = e.target.value
 }
 function set_flag(idx, flagid){
     document.getElementById("flag"+idx).attributes['xlink:href'].value = "#" + flagid
-    document.getElementById("flag"+idx+"input").style.background = "url(svg/"+flagid+".svg) repeat-x"
+    document.getElementById("flag"+idx+"input").getElementsByTagName("button")[0].style.background = "url(svg/"+flagid+".svg) repeat-x"
 }
 
 function switch_flags(){
@@ -16,4 +17,16 @@ function switch_flags(){
     f2 = document.getElementById("flag2").attributes['xlink:href'].value.slice(1)
     set_flag(1,f2)
     set_flag(2,f1)
+}
+
+function add_flag_to_dropdown(idx, flagname){
+    d = document.getElementById("flag"+idx+"input").getElementsByClassName("dropdown-content")[0]
+    d.innerHTML += "<a href='#' onclick='set_flag("+idx+",\""+flagname + "\")'><svg viewBox='0 0 400 400'><use xlink:href='#"+flagname+"'/><use xlink:href='#squareframe'/></svg></a>"
+}
+
+function build_flag_dropdowns(){
+    for (flag of document.getElementsByClassName("flag")){
+        add_flag_to_dropdown(1, flag.id)
+        add_flag_to_dropdown(2, flag.id)
+    }
 }
